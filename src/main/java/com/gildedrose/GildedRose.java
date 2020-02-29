8c31c33fe46a6d0dade0c532aa433c10ce4422de
Jin <jbjxu@thoughtworks.com>
2020-02-29 17:46:23 +0800

    refactor: move update quality as item method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -5,91 +5,11 @@ import java.util.Arrays;
 class GildedRose {
     Item[] items;
 
-    private final String AGED_BRIE = "Aged Brie";
-    private final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
-    private final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    private final int MAX_QUALITY = 50;
-    private final int MIN_QUALITY = 0;
-    private final int MIN_SELLIN = 0;
-
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
     public void updateQuality() {
-        Arrays.stream(this.items).forEach(this::updateQualitySingle);
-    }
-
-    private void updateQualitySingle(Item item) {
-        if (specialItem(item)) {
-            increaseQuality(item);
-        }
-
-        subtractQuality(item);
-        subtractSellIn(item);
-
-        if (greaterThanMinSellIn(item)) {
-            return;
-        }
-
-        if (increaseQualityAfterSellIn(item)) {
-            increaseQuality(item);
-        }
-
-        if (qualityBeZeroAfterSellIn(item)) {
-            item.quality = MIN_QUALITY;
-        }
-
-        subtractQuality(item);
-    }
-
-    private boolean specialItem(Item item) {
-        return increaseQualityAfterSellIn(item) || qualityBeZeroAfterSellIn(item);
-    }
-
-    private boolean qualityBeZeroAfterSellIn(Item item) {
-        return item.name.equals(BACKSTAGE_PASS);
-    }
-
-    private boolean increaseQualityAfterSellIn(Item item) {
-        return item.name.equals(AGED_BRIE);
-    }
-
-    private boolean greaterThanMinSellIn(Item item) {
-        return item.sell_in >= MIN_SELLIN;
-    }
-
-    private boolean greaterThanMaxQuality(Item item) {
-        return item.quality >= MAX_QUALITY;
-    }
-
-    private void subtractSellIn(Item item) {
-        if (noSellInWithSameQuality(item)) {
-            return;
-        }
-        item.sell_in = item.sell_in - 1;
-    }
-
-    private void increaseQuality(Item item) {
-        if (greaterThanMaxQuality(item)) {
-            return;
-        }
-        item.quality = item.quality + 1;
-    }
-
-    private void subtractQuality(Item item) {
-        if (noSellInWithSameQuality(item) || noQuality(item)) {
-            return;
-        }
-
-        item.quality = item.quality - 1;
-    }
-
-    private boolean noQuality(Item item) {
-        return item.quality <= MIN_QUALITY;
-    }
-
-    private boolean noSellInWithSameQuality(Item item) {
-        return item.name.equals(SULFURAS);
+        Arrays.stream(this.items).forEach(Item::updateQuality);
     }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -2,11 +2,18 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private final String AGED_BRIE = "Aged Brie";
+    private final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
+    private final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private final int MAX_QUALITY = 50;
+    private final int MIN_QUALITY = 0;
+    private final int MIN_SELLIN = 0;
 
-    public int sell_in;
+    private String name;
 
-    public int quality;
+    private int sell_in;
+
+    private int quality;
 
     public Item(String name, int sell_in, int quality) {
         this.name = name;
@@ -17,5 +24,78 @@ public class Item {
     @Override
     public String toString() {
         return this.name + ", " + this.sell_in + ", " + this.quality;
+    }
+
+    void updateQuality() {
+        if (specialItem()) {
+            increaseQuality();
+        }
+
+        subtractQuality();
+        subtractSellIn();
+
+        if (greaterThanMinSellIn()) {
+            return;
+        }
+
+        if (increaseQualityAfterSellIn()) {
+            increaseQuality();
+        }
+
+        if (qualityBeZeroAfterSellIn()) {
+            quality = MIN_QUALITY;
+        }
+
+        subtractQuality();
+    }
+
+    private boolean specialItem() {
+        return increaseQualityAfterSellIn() || qualityBeZeroAfterSellIn();
+    }
+
+    private boolean qualityBeZeroAfterSellIn() {
+        return name.equals(BACKSTAGE_PASS);
+    }
+
+    private boolean increaseQualityAfterSellIn() {
+        return name.equals(AGED_BRIE);
+    }
+
+    private boolean greaterThanMinSellIn() {
+        return sell_in >= MIN_SELLIN;
+    }
+
+    private void subtractSellIn() {
+        if (noSellInWithSameQuality()) {
+            return;
+        }
+        sell_in = sell_in - 1;
+    }
+
+    private void increaseQuality() {
+        if (greaterThanMaxQuality()) {
+            return;
+        }
+        quality = quality + 1;
+    }
+
+    private boolean greaterThanMaxQuality() {
+        return quality >= MAX_QUALITY;
+    }
+
+    private void subtractQuality() {
+        if (noSellInWithSameQuality() || noQuality()) {
+            return;
+        }
+
+        quality = quality - 1;
+    }
+
+    private boolean noQuality() {
+        return quality <= MIN_QUALITY;
+    }
+
+    private boolean noSellInWithSameQuality() {
+        return name.equals(SULFURAS);
     }
 }
